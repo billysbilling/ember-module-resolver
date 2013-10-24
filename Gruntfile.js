66010ca2886c6f92f444664b938e23436c642a30
Sebastian Seilund <sebastian@seilund.com>
2013-10-23 17:21:16 -0700

    added initial files

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        billy_builder: {
+            title: 'ember-module-resolver'
+        }
+    });
+
+    grunt.loadNpmTasks('billy-builder');
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# ember-module-resolver
