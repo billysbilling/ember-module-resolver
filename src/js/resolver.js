95024bab88dbddd32d9946fd3a730da72bcf108e
Sebastian Seilund <sebastian@seilund.com>
2013-10-23 17:34:50 -0700

    made rootDir configurable

--- src/js/resolver.js
+++ src/js/resolver.js
@@ -1,57 +1,59 @@
-module.exports = Ember.DefaultResolver.extend({
-    resolveTemplate: resolveTemplate,
-    resolveOther: resolveOther,
-    normalize: normalize
-});
+module.exports = function(rootDir) {
+    var resolveTemplate = function(parsedName) {
+        var moduleName = rootDir+'/templates/'+normalizeTemplateName(parsedName.name);
+        return require(moduleName, true);
+    };
 
-function resolveTemplate(parsedName) {
-    var moduleName = '../templates/'+normalizeTemplateName(parsedName.name);
-    return require(moduleName, true);
-}
+    var resolveOther = function(parsedName) {
+        var fullName = parsedName.fullName;
 
-function resolveOther(parsedName) {
-    var fullName = parsedName.fullName;
+        if (fullName === 'application:main') {
+            return null;
+        }
+        if (fullName === 'router:main') {
+            return require(rootDir+'/router');
+        }
 
-    if (fullName === 'application:main') {
-        return null;
-    }
-    if (fullName === 'router:main') {
-        return require('./router');
-    }
+        var type = parsedName.type,
+            name = parsedName.name,
+            moduleName = rootDir+'/'+type+'s/'+normalizeOtherName(name);
 
-    var type = parsedName.type,
-        name = parsedName.name,
-        moduleName = './'+type+'s/'+normalizeOtherName(name);
+        if (type === 'util') {
+            return null;
+        }
 
-    if (type === 'util') {
-        return null;
-    }
+        return require(moduleName, true);
+    };
 
-    return require(moduleName, true);
-}
+    var normalize = function(fullName) {
+        var split = fullName.split(':', 2),
+            type = split[0],
+            name = split[1];
 
-function normalize(fullName) {
-    var split = fullName.split(':', 2),
-        type = split[0],
-        name = split[1];
+        Ember.assert("Tried to normalize a container name without a colon (:) in it. You probably tried to lookup a name that did not contain a type, a colon, and a name. A proper lookup name would be `view:post`.", split.length === 2);
 
-    Ember.assert("Tried to normalize a container name without a colon (:) in it. You probably tried to lookup a name that did not contain a type, a colon, and a name. A proper lookup name would be `view:post`.", split.length === 2);
+        if (type !== 'template') {
+            return type + ':' + normalizeOtherName(name);
+        } else {
+            return fullName;
+        }
+    };
 
-    if (type !== 'template') {
-        return type + ':' + normalizeOtherName(name);
-    } else {
-        return fullName;
-    }
-}
+    var normalizeTemplateName = function(name) {
+        name = name.dasherize();
+        name = name.replace(/\./g, '/');
+        return name;
+    };
 
-function normalizeTemplateName(name) {
-    name = name.dasherize();
-    name = name.replace(/\./g, '/');
-    return name;
-}
-
-function normalizeOtherName(name) {
-    name = name.dasherize();
-    name = name.replace(/\./g, '-');
-    return name;
-}
+    var normalizeOtherName = function(name) {
+        name = name.dasherize();
+        name = name.replace(/\./g, '-');
+        return name;
+    };
+    
+    return Ember.DefaultResolver.extend({
+        resolveTemplate: resolveTemplate,
+        resolveOther: resolveOther,
+        normalize: normalize
+    });
+};
